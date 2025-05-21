00b502fa228ef63b546984e1a5da224f5f422c3d
amollrod <alex.moll.rodriguez@gmail.com>
2025-05-21 20:01:57 +0200

    test para todos los cors

--- src/main/java/com/tfg/auth/infrastructure/config/AuthorizationSecurityConfig.java
+++ src/main/java/com/tfg/auth/infrastructure/config/AuthorizationSecurityConfig.java
@@ -103,7 +103,7 @@ public class AuthorizationSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(allowedOrigins));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
